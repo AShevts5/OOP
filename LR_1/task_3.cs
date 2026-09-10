72330c42136a32c296dfc6be600aa996dc3921f3
Anna_Shevtsova9775 <203544446+AShevts5@users.noreply.github.com>
2026-09-11 02:30:15 +0300

    опа первая лаба

---
+++ LR_1/task_3.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OOP {
+    class task_3 {
+        static void Main() {
+            Console.Write("Введите зарплату: ");
+            int salary = Convert.ToInt32(Console.ReadLine());
+            double tax = salary * 13 / 100;
+            Console.WriteLine("Сумма подоходного налога = " + tax);
+        }
+    }
+}
