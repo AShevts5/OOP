bc0d7d87edce332ae23b6947b53942d89b28b133
Anna_Shevtsova9775 <203544446+AShevts5@users.noreply.github.com>
2026-09-11 08:53:11 +0300

    Update LR_2.cs

--- LR_1/LR_2.cs
+++ LR_1/LR_2.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace OOP {
     class LR_2 {
-        static void Main() {
+        static void Main(string[] args) {
             int summ = 0; 
             for (int i = 15; i < 90; i++) {
                 if (i % 2 == 0) {
