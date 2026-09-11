2834cc302096b5d29fc3a71986de4aa682b3fff1
Anna_Shevtsova9775 <203544446+AShevts5@users.noreply.github.com>
2026-09-11 08:54:06 +0300

    Update task_1.cs

--- LR_1/task_1.cs
+++ LR_1/task_1.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace OOP {
     class task_1 {
-        static void Main() {
+        static void Main(string[] args) {
             Console.WriteLine("Нет победителя сильнее того, кто сумел победить самого себя.");
         }
     }
