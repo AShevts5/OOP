bc3f11cf840e6594b578915da3e05fcd1f392089
Anna_Shevtsova9775 <203544446+AShevts5@users.noreply.github.com>
2026-09-19 16:48:01 +0300

    и ещё поправочка

--- LR_1/task_2_4.cs
+++ LR_1/task_2_4.cs
@@ -1,24 +1,37 @@
 using System;
 using static System.Math;
 
-namespace OOP {
-    class task_2 {
-        static void Main(string[] args) {
-            double Y, x;
-            
-            Console.Write("Введите x: ");
-            while (!double.TryParse(Console.ReadLine(), out x))
-            {
-                Console.Write("Окак, ошибочка! Введите число ещё раз: ");
-            }
+namespace OOP;
 
-            // числитель
-            double c = Sin(2*x) * Cos(x) - 2.4 * Log(x) - Tan(x);
-            // знаменатель
-            double z = Sin(x) * Cos(x) - (Pow(x, 2)/Abs(x));
+class task_2_4
+{
+    static void Main(string[] args)
+    {
+        double Y, x;
 
-            Y = c/z;
-            Console.Write("Y = " + Y);
+        Console.Write("Введите x: ");
+        while (!double.TryParse(Console.ReadLine(), out x))
+        {
+            Console.Write("Окак, ошибочка! Введите число ещё раз: ");
+        }
+
+        if (x <= 0)
+        {
+            Console.WriteLine("Ошибка: x должен быть больше нуля");
+            return;
+        }
+
+        double c = Sin(2 * x) * Cos(x) - 2.4 * Log(x) - Tan(x);
+        double z = Sin(x) * Cos(x) - (Pow(x, 2) / Abs(x));
+
+        if (z == 0)
+        {
+            Console.WriteLine("Ошибка: деление на ноль");
+        }
+        else
+        {
+            Y = c / z;
+            Console.WriteLine($"Y = {Y}"); 
         }
     }
 }
