1ca56356970502c38f3a7de5716309235a3a1188
Anna_Shevtsova9775 <203544446+AShevts5@users.noreply.github.com>
2026-09-20 20:03:08 +0300

    честно последняя правка

--- LR_1/task_2_1.cs
+++ LR_1/task_2_1.cs
@@ -16,9 +16,18 @@ namespace OOP
                     Console.Write("Окак, ошибочка! Введите число ещё раз: ");
                 };
 
+                // подкоренное выражение в знаменателе дроби 1
+                var p = 4*Pow(x, 2) - 3*x + 1;
+
+                if (p < 0)
+                {
+                    Console.WriteLine("Ошибка: подкоренное выражение отрицательно. Попробуйте другое значение x.");
+                    continue;
+                }
+
                 // дробь 1
                 c1 = 2*x - Pow(3, x);
-                z1 = Sqrt(4*Pow(x, 2) - 3*x + 1);
+                z1 = Sqrt(podkorennoe);
 
                 // дробь 2
                 c2 = Pow(3, x+1) + Pow(3, x-2) + 2;
@@ -27,12 +36,12 @@ namespace OOP
                 if (Abs(z1) < eps || Abs(z2) < eps)
                 {
                     Console.WriteLine("Ошибка: деление на ноль при данном значении x. Попробуйте другое значение.");
-                    continue; 
+                    continue;
                 }
 
                 Y = c1/z1 + c2/z2;
                 Console.Write($"Y = {Y}");
-                break; 
+                break;
             }
         }
     }
