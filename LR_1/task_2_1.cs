340d96141406bd2a994d72523eef787d1e6e4e2a
Anna_Shevtsova9775 <203544446+AShevts5@users.noreply.github.com>
2026-09-20 19:46:27 +0300

    вспомниль про ОДЗ

--- LR_1/task_2_1.cs
+++ LR_1/task_2_1.cs
@@ -5,28 +5,35 @@ namespace OOP
 {
     class task_2_1 {
         static void Main(string[] args) {
-            double x;
-            
-            Console.Write("Введите x: ");
-            while (!double.TryParse(Console.ReadLine(), out x))
+            const double eps = 1e-9;
+            double x, c1, z1, c2, z2, Y;
+
+            while (true)
             {
-                Console.Write("Окак, ошибочка! Введите число ещё раз: ");
-            };
+                Console.Write("Введите x: ");
+                while (!double.TryParse(Console.ReadLine(), out x))
+                {
+                    Console.Write("Окак, ошибочка! Введите число ещё раз: ");
+                };
 
-            // дробь 1
-            // числитель
-            var c1 = 2*x - Pow(3, x);
-            // знаменатель
-            var z1 = Sqrt(4*Pow(x, 2) - 3*x + 1);
+                // дробь 1
+                c1 = 2*x - Pow(3, x);
+                z1 = Sqrt(4*Pow(x, 2) - 3*x + 1);
 
-            // дробь 2
-            // числитель
-            var c2 = Pow(3, x+1) + Pow(3, x-2) + 2;
-            var z2 = Pow(Sin(2+Pow(x, 2)), 2);
+                // дробь 2
+                c2 = Pow(3, x+1) + Pow(3, x-2) + 2;
+                z2 = Pow(Sin(2+Pow(x, 2)), 2);
 
-            var Y = c1/z1 + c2/z2;
+                if (Abs(z1) < eps || Abs(z2) < eps)
+                {
+                    Console.WriteLine("Ошибка: деление на ноль при данном значении x. Попробуйте другое значение.");
+                    continue; 
+                }
 
-            Console.Write($"Y = {Y}");
+                Y = c1/z1 + c2/z2;
+                Console.Write($"Y = {Y}");
+                break; 
+            }
         }
     }
 }
