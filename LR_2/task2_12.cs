6b0dfb8d507934713e4821d31b2f5d4308ea1546
Anna_Shevtsova9775 <203544446+AShevts5@users.noreply.github.com>
2026-09-22 15:31:23 +0300

    особенно после работы на нём 10 часов 3 дня подряд

---
+++ LR_2/task2_12.cs
@@ -0,0 +1,29 @@
+using System;
+using static System.Console;
+using static System.Math; 
+
+namespace OOP 
+{
+    class Task2_12
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Write($"Введите {i}-й элемент массива: "); 
+                arr[i] = Convert.ToDouble(ReadLine());
+            }
+            
+            int cnt = 0;
+            foreach (double i in arr)
+            {
+                cnt += (i > 0 && i % 2 == 0) ? 1 : 0;
+            }
+
+            WriteLine($"Результат: {cnt}");
+            
+        }
+    }
+}
